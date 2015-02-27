8fe02d0b7d0c025ad68ffa63af13716d6a41fbef
Max Dietz <mdietz@wesleyan.edu>
2015-02-27 02:54:22 +0000

    weeeeeeee

--- send-contact-message.php
+++ send-contact-message.php
@@ -1,37 +1,34 @@
 <?php
-if(!isset($_POST['submit']))
-{
-#This page should not be submitted directly. Need to submit the form.
-	echo "error; you need to submit the form";
-}
+require_once 'Mail.php';
+
 $fname = $_POST['firstname'];
 $lname = $_POST['lastname'];
 $visitor_email = $_POST['email'];
 $location = $_POST['location'];
 $phnumber = $_POST['phonenumber'];
 $message = $_POST['message'];
+$headers = array(
+    'From' => '<kakoi@joomah.com>',
+    'To' => '<'.$visitor_email.'>',
+    'Subject' => 'New Form Submission'
+);
 
-#Validate first
-if(empty($fname)||empty($lname)||empty($location)||empty($phnumber)||empty($message))
-{
-	echo "Your name, email, phone number and location are mandatory";
-	exit();
-}
-
-$email_from = 'user@raphaphysio.com'; #Client's sending email address.
-$email_subject = "New Form Submission";
-$email_body = "You have received a new message from $fname. \n"
-			  "Name: $fname\n"
-			  "Email: $visitor_email\n"
-			  "Location: $location\n"
-			  "Phone Number: $phnumber\n"
+$email_body = "You have received a new message from $fname. \n" .
+			  "Name: $fname\n" .
+			  "Email: $visitor_email\n" .
+			  "Location: $location\n" .
+			  "Phone Number: $phnumber\n" .
 			  "Message:\n $message";
 
-$to = "user@raphaphysio.com"; #Client's receiving email address
-$headers = "From: $email_from \r\n";
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'kakoi@joomah.com',
+        'password' => 'ardelle1992'
+));
 
-#Send the message
-mail($to,$email_subject,$email_body,$headers);
+$mail = $smtp->send($visitor_email,$headers,$email_body);
 
 #Done. Redirect to thank you page
 header('Location: contact-thank-you.html');
